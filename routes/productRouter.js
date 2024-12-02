612896358e4a7cfc40ae5ee79fd5658a3d275c1b
Dulakshi RB <dulakshirb@gmail.com>
2024-12-02 11:51:33 +0530

    Fixed productRouter error

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { createProduct, getProduct } from "../controllers/productController.js";
+import {
+  createProduct,
+  getProducts,
+} from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
